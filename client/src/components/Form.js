5bfed859bb964de20d5ebb5f868311fcfe184857
Alisa Poon <alisa.poon97@gmail.com>
2020-10-29 22:25:27 -0700

    F - form can send email from portfolio page

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
-export default function LayoutTextFields() {
+export default function Form() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [text, setText] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:8080/email',{
+      email,
+      subject: `${name}: ${subject}`,
+      text,
+    })
+      .then(res => {
+        setName('');
+        setEmail('');
+        setSubject('');
+        setText('');
+      });
+  }
 
   return (
       <form>
         <TextField
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           label="Name"
           fullWidth
           margin="normal"
@@ -16,6 +39,8 @@ export default function LayoutTextFields() {
           variant="outlined"
         />
         <TextField
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           label="Email"
           fullWidth
           margin="normal"
@@ -25,6 +50,8 @@ export default function LayoutTextFields() {
           variant="outlined"
         />
         <TextField
+          name={subject}
+          onChange={(e) => setSubject(e.target.value)}
           label="Subject"
           fullWidth
           margin="normal"
@@ -34,6 +61,8 @@ export default function LayoutTextFields() {
           variant="outlined"
         />
         <TextField
+          name={text}
+          onChange={(e) => setText(e.target.value)}
           label="Message"
           fullWidth
           margin="normal"
@@ -44,7 +73,7 @@ export default function LayoutTextFields() {
           multiline
           rows={5}
         />
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary" onClick={handleSubmit}>
           Submit
         </Button>
       </form>
