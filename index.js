e88b2b755a068a4436505cea1305628027578900
Alisa Poon <alisa.poon97@gmail.com>
2020-11-30 17:23:30 -0800

    add more to index.js again

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 8080;
 const sendMail = require('./mail.js');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
