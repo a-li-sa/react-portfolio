3f5878f0ea12887cd54482d1ca228c1a41f2547f
Alisa Poon <alisa.poon97@gmail.com>
2020-11-30 16:35:14 -0800

    change index.js so that application can be deployed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const sendMail = require('./mail.js');
 
 app.use(express.urlencoded({extended:true}));
